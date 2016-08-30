79ddafe217bb2dd3a366856bb3b19dcb1fc6c525
Kouichi Matsuoka <dragonko15296@gmail.com>
2016-08-30 07:22:10 +0000

    retake2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+n = 1
+while n < 101
+  Blog.create(
+    title: "負けないでもう少し最後まで走り抜けて",
+    content: "どんなに離れてても心はそばにいるわ。追いかけてはるかな夢を・・・。どぅくどぅくどぅくどぅく"
+    user_id: n
+  )
+  n = n + 1
+end
