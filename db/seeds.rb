70b41326ba8708587fb3fdbfec7749c602698a9f
Kouichi Matsuoka <dragonko15296@gmail.com>
2016-08-30 07:55:43 +0000

    retake4

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 100.times do |n|
   email = Faker::Internet.email
+  name = Faker::Name.name
   password = "password"
   User.create!(email: email,
                password: password,
                password_confirmation: password,
+               name: name
                )
 end
 n = 1
 while n < 101
   Blog.create(
-    title: "負けないでもう少し最後まで走り抜けて",
-    content: "どんなに離れてても心はそばにいるわ。追いかけてはるかな夢を・・・。どぅくどぅくどぅくどぅく",
+    title: "負けないで！もう少し、最後まで、走り抜けて。",
+    content: "どんなに離れてても、心はそばにいるわ。追いかけてはるかな夢を・・・。どぅくどぅくどぅくどぅく",
     user_id: n
   )
   n = n + 1
