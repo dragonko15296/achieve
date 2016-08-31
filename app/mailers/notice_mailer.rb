00be8642d2bd708b949a263694f27fce2b08366e
Kouichi Matsuoka <dragonko15296@gmail.com>
2016-08-31 08:09:55 +0000

    DIVE10

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -9,6 +9,12 @@ class NoticeMailer < ApplicationMailer
     @blog = blog
 
     mail to: "dragonko15296@gmail.com",
-         subject: '【Achieve】ブログが投稿されました'
+         subject: '【Achieve】記事が投稿されました'
+  end
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: "dragonko15296@gmail.com",
+         subject: '【Achieve】お問い合わせがありました'
   end
 end
