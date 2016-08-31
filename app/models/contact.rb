0779c74b6107c1b4faff2757dae8078e4871c1ae
Kouichi Matsuoka <dragonko15296@gmail.com>
2016-08-31 02:57:49 +0000

    fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
     validates :name,:content, presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
